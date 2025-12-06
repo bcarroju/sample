3d1c8e25967a9f7178ca066fb860db9bdbfc6a1a
bcarroju <bashudevops@gmail.com>
2025-12-06 14:08:32 -0600

    Updated test.py for Jira_001 fix

--- test.py
+++ test.py
@@ -1,1 +1,55 @@
+# test.py
+
+# 1. Hello World
+print("Hello, World!")
+
+# 2. Variables & Data Types
+name = "Bhaskar"
+age = 40
+is_devops = True
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"DevOps Engineer: {is_devops}")
+
+# 3. List & Loop
+tools = ["AWS", "Docker", "Kubernetes", "Terraform", "Jenkins"]
+
+print("\nDevOps Tools:")
+for tool in tools:
+    print(f"- {tool}")
+
+# 4. Function
+def greet(user):
+    return f"\nWelcome to Python, {user}!"
+
+print(greet(name))
+
+# 5. Math Operations
+a = 10
+b = 5
+print("\nMath Operations:")
+print(f"Addition: {a + b}")
+print(f"Subtraction: {a - b}")
+print(f"Multiplication: {a * b}")
+print(f"Division: {a / b}")
+
+# 6. Dictionary
+server = {
+    "name": "prod-server-1",
+    "ip": "10.0.0.25",
+    "env": "production"
+}
+
+print("\nServer Details:")
+for key, value in server.items():
+    print(f"{key}: {value}")
+
+# 7. User Input
+user_name = input("\nEnter your name: ")
+print(f"Hello, {user_name}! Python is running successfully ✅")
+
+# 8. If-Else Condition
+if age >= 18:
+    p
 print("Hello, World!")
